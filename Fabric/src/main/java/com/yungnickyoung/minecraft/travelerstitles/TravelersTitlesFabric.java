263055841970c34bc2ee3280570cd8529e56c646
hesa2020 <hesawisa@hotmail.com>
2025-12-06 13:57:37 -0500

    ...

--- Fabric/src/main/java/com/yungnickyoung/minecraft/travelerstitles/TravelersTitlesFabric.java
+++ Fabric/src/main/java/com/yungnickyoung/minecraft/travelerstitles/TravelersTitlesFabric.java
@@ -1,10 +1,20 @@
 package com.yungnickyoung.minecraft.travelerstitles;
 
+import com.yungnickyoung.minecraft.travelerstitles.module.CommandModule;
+import com.yungnickyoung.minecraft.travelerstitles.module.SoundModule;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class TravelersTitlesFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Register sounds
+        SoundModule.init();
+
+        // Register commands
+        CommandRegistrationCallback.EVENT.register(CommandModule::registerCommands);
+
+        // Initialize common
         TravelersTitlesCommon.init();
     }
 }
