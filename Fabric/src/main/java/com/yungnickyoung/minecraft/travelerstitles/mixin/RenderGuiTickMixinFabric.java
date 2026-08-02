8626f556fdd54a9202e0ad11df9cd984e2a64f62
MentalCokuntus <112826899+MentalCokuntus@users.noreply.github.com>
2026-08-02 09:48:43 +0300

    Port to Minecraft 26.2

--- Fabric/src/main/java/com/yungnickyoung/minecraft/travelerstitles/mixin/RenderGuiTickMixinFabric.java
+++ Fabric/src/main/java/com/yungnickyoung/minecraft/travelerstitles/mixin/RenderGuiTickMixinFabric.java
@@ -2,14 +2,14 @@ package com.yungnickyoung.minecraft.travelerstitles.mixin;
 
 import com.yungnickyoung.minecraft.travelerstitles.TravelersTitlesCommon;
 import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.Hud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Gui.class)
+@Mixin(Hud.class)
 public class RenderGuiTickMixinFabric {
     @Inject(method = "extractRenderState", at = @At(value = "HEAD"))
     private void travelerstitles_onClientTick(GuiGraphicsExtractor guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci) {
